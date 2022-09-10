74f1b50ffe8ad8ef6748f47b7b541b5bd857b6d0
EddieDevv <eddieagic2001@gmail.com>
2022-09-10 12:31:42 -0500

    project setup

---
+++ src/components/animatedroutes.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Routes, Route, useLocation } from "react-router-dom"
+import { AnimatePresence } from "framer-motion"
+import Home from "../pages/home"
+
+function AnimatedRoutes() {
+    const location = useLocation();
+
+    return (
+        <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </AnimatePresence>
+    )
+}
+
+export default AnimatedRoutes
